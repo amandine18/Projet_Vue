b3074722b0ff48ad5bcd8991670a0de3894aa1c7
Amandine DURAND <durandamandine@icloud.com>
2024-12-06 11:15:11 +0100

    update master

--- src/components/ApiComponent.vue
+++ src/components/ApiComponent.vue
@@ -7,7 +7,7 @@
                     <li>
                         <div class="field">
                             <label for="url">URL :</label>
-                            <input type="text" id="url" v-model="apiUrl" @input="clearError('errorUrl')" />
+                            <input type="text" id="url" v-model="apiUrl" @input="handleUrlChange()"/>
                         </div>
                         <div class="erreur">
                             <span class="error" v-if="errorUrl">{{ errorUrl }}</span>
@@ -16,13 +16,21 @@
                     <li>
                         <div class="field">
                             <label for="methode">Méthode :</label>
-                            <input type="text" id="methode" v-model="apiMethod" />
+                            <select id="methode" v-model="apiMethod">
+                                <option value="GET">GET</option>
+                                <option value="POST">POST</option>
+                                <option value="PUT">PUT</option>
+                                <option value="DELETE">DELETE</option>
+                            </select>
                         </div>
                     </li>
-                    <li>
+                    <li v-if="apiMethod !== 'GET' && apiMethod !== 'DELETE'">
                         <div class="field">
                             <label for="param">Paramètre :</label>
-                            <input type="text" id="param" v-model="apiParam" />
+                            <textarea id="param" v-model="apiParam" @input="handleMethodChange()" />
+                        </div>
+                        <div class="erreur">
+                            <span class="error" v-if="errorParam">{{ errorParam }}</span>
                         </div>
                     </li>
                 </ul>
@@ -41,6 +49,7 @@
 </template>
 
 <script>
+// import axios from 'axios';
 export default {
     data() {
         return {
@@ -49,15 +58,22 @@ export default {
             apiParam: '',
             apiResponse: null, // Stocke la réponse de l'API
             errorUrl: null,
+            errorParam: null,
         };
     },
     methods: {
         validateForm() {
             let isValid = true;
             this.errorUrl = null;
+            this.errorParam = null;
 
-            if (!this.apiUrl){
+            if (!this.apiUrl) {
                 this.errorUrl = "L'URL est requise";
+                isValid = false;
+            }
+
+            if (((this.apiMethod !== 'GET') && (this.apiMethod !== 'DELETE')) && (!this.apiParam)) {
+                this.errorParam = "Le paramètre est requis et doit être un JSON valide";
                 isValid = false;
             }
 
@@ -68,29 +84,105 @@ export default {
             if (field === 'errorUrl') {
                 this.errorUrl = null;
             }
+            if (field === 'errorParam') {
+                this.errorParam = null;
+            }
+        },
+        handleUrlChange() {
+            this.apiResponse = null; // Réinitialise la réponse lorsque l'URL est modifiée
+            this.clearError('errorUrl');
+        },
+        handleMethodChange() {
+            this.apiResponse = null;
+            this.clearError('errorParam');
+        },
+        isValidJson(str) {
+            try {
+                JSON.parse(str);
+                return true;
+            } catch (e) {
+                return false;
+            }
         },
         async fetchApi() {
-            if (!this.validateForm()) {
-                return; // Arrête la fonction si la validation échoue
-            }
+            // if (!this.validateForm()) {
+            //     return; //Arrête la fonction si la validation échoue
+            // }
+            // let url = this.apiUrl
+            // let options = { method: this.apiMethod };
+            // if (this.apiMethod !== 'GET') {
+            //     options.headers = { 'Content-Type': 'application/json' }, {'accept': 'application/json'}, {'X-API-KEY': this.apiKey} ;
+            //     options.body = JSON.stringify(JSON.parse(this.apiParam));
+            //     console.log('test3')
+            // }
+
+            // const response = await fetch(url, options);
+            // console.log('test5')
+            // if (!response.ok) {
+            //     throw new Error(`Erreur HTTP : ${response.status}`);
+            // }
+
+            // const data = await response.json();
+            // this.apiResponse = JSON.stringify(response, null, 2); 
+            // try {
             
-            try {
-                let url = this.apiUrl;
-                if (this.apiParam) {
-                    url += `?${this.apiParam}`;
-                }
+            // } catch (error) {
+            //     console.error(error);
+            //     // this.apiResponse = "Aucune réponse de l'API, réessayez avec d'autres informations";
+            //     this.apiResponse = error;
+            // }
 
-                const response = await fetch(url, { method: this.apiMethod });
-                if (!response.ok) {
-                    throw new Error(`Erreur HTTP : ${response.status}`);
-                }
+            let url = this.apiUrl;
+            let option = this.apiMethod;
+            let param = this.apiParam;
 
-                const data = await response.json(); // Supposons que la réponse soit au format JSON
-                this.apiResponse = JSON.stringify(data, null, 2); // Formatage JSON pour l'affichage
-            } catch (error) {
-                console.error(error);
-                this.apiResponse = "Aucune réponse de l'API, réessayez avec d'autres informations";
+            let requete = new XMLHttpRequest();
+            requete.onreadystatechange = () => {
+                if (requete.readyState === XMLHttpRequest.DONE) {
+                    if (requete.status === 200 || requete.status === 201) {
+                        try {
+                            // Parse and set the API response to the variable
+                            this.apiResponse = JSON.parse(requete.responseText);
+                        } catch (error) {
+                            console.error("JSON Parsing Error:", error.message);
+                            this.apiResponse = "Erreur lors de l'analyse de la réponse JSON.";
+                        }
+                    } else {
+                        console.error(`HTTP Error: ${requete.status} - ${requete.statusText}`);
+                        console.log(requete.readyState);
+                        this.apiResponse = `Erreur HTTP: ${requete.status} - ${requete.statusText}`;
+                    }
+                }
+            };
+
+            requete.open(option, url);
+            if (this.apiMethod !== 'GET' && this.apiMethod !== 'DELETE') {
+                const payload = typeof param === 'string' ? JSON.parse(param) : param;
+                requete.setRequestHeader("Content-Type", "application/json");
+                requete.send(JSON.stringify(payload));
+            } else {
+                requete.send();
             }
+
+
+            // try {
+            //     const config = {
+            //         method: this.apiMethod,
+            //         url: this.apiUrl,
+            //         headers: {
+            //             'Content-Type': 'application/json',
+            //         }
+            //     };
+
+            //     if (this.apiMethod !== 'GET' && this.apiMethod !== 'DELETE') {
+            //         config.data = JSON.parse(this.apiParam);
+            //     }
+
+            //     const response = await axios(config);
+            //     this.apiResponse = JSON.stringify(response.data, null, 2);
+            // } catch (error) {
+            //     this.apiResponse = `Erreur: ${error.response?.data || error.message}`;
+            // }
         },
     },
 }
